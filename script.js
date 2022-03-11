ee4ce3ede562f82503cf9a8b0db820240e2357d3
Jon Alpha <Death77485@aol.com>
2022-03-11 11:21:20 -0600

    Added cuboid movement control functionality

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+// Cube 
+
+const cube = document.querySelector('.cube');
+let x = 0;
+let y = 0;
+let z = 0;
+let bool = true;
+let interval;
+
+document.querySelector(".top-x-control").addEventListener("click", () => {
+    cube.style.transform = `rotateX(${x += 20}deg) rotateY(${y}deg) rotateZ(${z}deg)`
+});
+
+document.querySelector(".bottom-x-control").addEventListener("click", () => {
+    cube.style.transform = `rotateX(${x -= 20}deg) rotateY(${y}deg) rotateZ(${z}deg)`
+});
+
+document.querySelector(".left-y-control").addEventListener("click", () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y -= 20}deg) rotateZ(${z}deg)`
+});
+
+document.querySelector(".right-y-control").addEventListener("click", () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y += 20}deg) rotateZ(${z}deg)`
+});
+
+document.querySelector(".top-z-control").addEventListener("click", () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z -= 20}deg)`
+});
+
+document.querySelector(".bottom-z-control").addEventListener("click", () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z += 20}deg)`
+});
+
+const playPause = () => {
+    if (bool) {
+        interval = setInterval(() => {
+            cube.style.transform = `rotateX(${x}deg) rotateY(${y++}deg) rotateZ(${z}deg)`
+        }, 100);
+    } else {
+        clearInterval(interval);
+    }    
+};
+
+playPause();
+
+document.querySelector('.controls').addEventListener("mouseover", () => {
+    bool = false;
+    playPause();
+});
+document.querySelector('.controls').addEventListener("mouseout", () => {
+    bool = true;
+    playPause();
+});
+
 // Slideshow
 
 // Adding div elements with background image style
@@ -34,3 +88,4 @@ const slideshow = () => {
 }
 
 slideshow();
+
